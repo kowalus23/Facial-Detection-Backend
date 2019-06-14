b7c547af2e274c6e1020c01d4a9f33be2db9815a
kowalus23 <kowalus23@gmail.com>
2019-06-14 20:44:37 +0200

    created new folder for all my endpoints, to make server.js much cleaner

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ In the project directory, you can run:
 ### `npm install`
 to install all packages that i used (especially nodemon)
 
-then to run server use: (by default it's set on port:3000)
+then to run server use(by default it's set on port:3000): 
 ### `npm start`
 
 Also be sure, that your port on front app is correct with listen() at the bottom of server.js file
--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const bcrypt = require('bcrypt');
 const cors = require('cors');
 const knex = require('knex');
 
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
 const db = knex({
   client: 'pg',
   version: '7.2',
@@ -15,92 +20,16 @@ const db = knex({
   }
 });
 
-db.select('*').from('users').then(data => {
-});
-
 const app = express();
+
 app.use(bodyParser.json());
 app.use(cors());
-app.get('/', (req, res) => {
-  res.send(database.users);
-});
 
-// SIGN IN end point//
-app.post('/signin', (req, res) => {
-  db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        db.select('*').from('users')
-          .where('email', '=', req.body.email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('unable to get user'))
-      } else {
-        res.status(400).json('wrong credentials')
-      }
-    })
-    .catch(err => res.status(400).json('wrong credentials'))
-});
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
+app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)});
+app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 
-/// REGISTER end point ///
-app.post('/register', (req, res,) => {
-  const {email, name, password} = req.body;
-  const hash = bcrypt.hashSync(password, 10);
-  db.transaction(trx => {
-    trx.insert({
-      hash: hash,
-      email: email
-    })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0])
-          })
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
-  })
-    .catch(err => res.status(400).json('unable to register'))
-});
-
-// PROFILE/IDs for future extends //
-app.get('/profile/:id', (req, res) => {
-  const {id} = req.params;
-  db.select('*').from('users').where({id})
-    .then(user => {
-      if (user.length) {
-        res.json(user[0]);
-      } else {
-        res.status(400).json('not found')
-      }
-    })
-    .catch(err => res.status(400).json('not found'))
-});
-
-//  UPDATE an IMAGE and entries value end //
-app.put('/image', (req, res) => {
-  const {id} = req.body;
-  db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-      res.json(entries[0]);
-    })
-    .catch(err => res.status(400).json('unable to get entries'))
-});
-
-// SERVER PORT LISTENER //
-app.listen(3002, () => {
-  console.log('app is running on port 3002');
+app.listen(3004, () => {
+  console.log('app is running on port 3004');
 });
