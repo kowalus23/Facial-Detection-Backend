abcea338f475c124fc02bc3d9c8b38d48a74e6bd
kowalus23 <kowalus23@gmail.com>
2019-06-17 14:50:39 +0200

    updated PORT for deploying on heroku

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)});
 app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 
-
-app.listen(3004, () => {
-  console.log('app is running on port 3004');
+const PORT = process.env.PORT;
+app.listen(PORT || 3007, () => {
+  console.log(`app is running on port ${PORT}`);
 });
